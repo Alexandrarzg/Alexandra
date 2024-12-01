cd3cd5d2fadbc17451bd00aecebeca5862a4a536
Alexandrarzg <alexa123gomez@outlook.es>
2024-11-30 18:14:17 -0600

    Create programa6.py

---
+++ Codigo/bloque2/programa6.py
@@ -0,0 +1,8 @@
+# P.6  negative subscript indices
+# 24/10/24 
+# Alexandra Rodriguez Gomez
+word = "hamster"
+print(word [ -1 ] ) 
+print(word [ -1:1 ] ) 
+print(word [ -3: ] ) 
+print(word [ :3 ] ) 
