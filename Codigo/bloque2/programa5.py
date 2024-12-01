bd029986ea24adb1e91248c69e381806e84930c9
Alexandrarzg <alexa123gomez@outlook.es>
2024-11-30 18:13:51 -0600

    Create programa5.py

---
+++ Codigo/bloque2/programa5.py
@@ -0,0 +1,23 @@
+# P.5 Comparacion , Membership , indexing , metodo de slicing
+# 23/10/2024
+# Alexandra Rodriguez Gomez.
+# COMPARACION
+print("perro" == "perro") #true
+print("perro" != "gato") #true
+print("Aguascalientes" < "Zacatecas") #true
+print("Aire" >= "Agua") #true
+print("\n ")
+# MEMBERSHIP
+print("house" in "boathouse") #true
+print("bien" in "bienvenidos") #true
+print("y" not in "ejes") #true
+print("je" in "ejes") #true
+print("\n ")
+# INDEXING(indexado)
+mi_nombre = "Alexandra"
+print(mi_nombre [3])
+print(mi_nombre [2])
+print("\n ")
+# SUBSCRIPTING 
+nombre = "Alexandra Rodriguez"
+print(nombre [2:6])
