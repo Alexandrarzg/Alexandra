ec66f4478a4694b8ef4e0b5541136db7c05f07de
Alexandrarzg <alexa123gomez@outlook.es>
2024-11-30 18:13:19 -0600

    Create programa4.py

---
+++ Codigo/bloque2/programa4.py
@@ -0,0 +1,7 @@
+# P.4 Programa que calcule los impuestos de un valor.
+# 22/10/2024
+# Alexandra Rodrguez Gomez.
+salario = int(input("Ingrese su salario: "))
+impuestos = salario * 0.16
+print("los impuestos son:" + str(impuestos) + " pesos" + "\n ¡Gracias por usar nuestro sistema!")
+
