934b80db652e4eca9aef536d5cfcf5bbaa91e26b
Alexandrarzg <alexa123gomez@outlook.es>
2024-11-30 18:09:26 -0600

    Create programa1_8.py

---
+++ Codigo/bloque 1/programa1_8.py
@@ -0,0 +1,10 @@
+# programa 1.8 calcular el numero de minutos, horas, meses dando el numero de dias
+# fecha: 16/10/2024
+# nombre: Alexandra Rodriguez Gomez
+dias = int(input("ingrese los dias: "))
+horas = dias * 24
+minutos = horas * 60
+meses = dias / 30
+print("las horas de los dias son: " + str(horas))
+print("los minutos de los dias son: " + str(minutos))
+print("los meses de los dias son: " + str(meses))
