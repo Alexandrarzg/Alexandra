1bd323114c235aab8d8f3ed1dcbdff1e18ad4c2f
Alexandrarzg <alexa123gomez@outlook.es>
2024-11-30 18:14:43 -0600

    Create programa7.py

---
+++ Codigo/bloque2/programa7.py
@@ -0,0 +1,8 @@
+# P.7 solicite al usuario la edad e indique si puede entrar a un bar
+# 24/10/24 
+# Alexandra Rodriguez Gomez
+edad = int(input("Ingrese la edad: "))
+if edad>=18:
+    print("Puede ingresar al bar")
+else:
+    print("ve a tu casa")
