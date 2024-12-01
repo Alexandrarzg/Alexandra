93035544102b483a71975a6b36a3569ea5f497a0
Alexandrarzg <alexa123gomez@outlook.es>
2024-11-30 18:11:44 -0600

    Create programa2.py

---
+++ Codigo/bloque2/programa2.py
@@ -0,0 +1,9 @@
+# P.2 programa Negacion de OR ejemplo: fiesta
+# 22/10/24
+# Hecho por: Alexandra Rodriguez Gomez
+# / A / B // NOT(A OR B) /
+# / 0 / 0 // 1
+print (not(False or False)) # Si hay fiesta
+print (not(False or True))  # No hay fiesta
+print (not(True or False))  # No hay fiesta
+print (not(True or True))  # No hay fiesta
