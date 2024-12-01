8e2359c37258ad04107068ac1749197d44d43ecc
Alexandrarzg <alexa123gomez@outlook.es>
2024-11-30 18:12:36 -0600

    Create programa3.py

---
+++ Codigo/bloque2/programa3.py
@@ -0,0 +1,5 @@
+# P.3 Programa que comprueba el funcionamiento de la adicion o concatenacion de varias cadenas de texto (strings)
+# 22/10/24
+# Hecho por: Alexandra Rodriguez Gomez
+
+print("Mi " + "nombre " + "es " + "Alexandra")
