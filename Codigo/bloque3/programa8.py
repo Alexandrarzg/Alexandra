a1f6c073cf5331b49219af10dd8adefb83ebbf9a
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:31:53 -0600

    Create programa8.py

---
+++ Codigo/bloque3/programa8.py
@@ -0,0 +1,14 @@
+# P-8 Operaciones con listas 
+# 31/10/24
+# Alexandra Rodriguez Gomez
+print("Indexing")
+numeros = [10, 20, 30]
+print(numeros [2])
+print(numeros [0])
+print(numeros [1])
+print(numeros [-1])
+print("In Subscripting")
+nombre = ["Juan", "Pedro", "Pepe el toro", "Emiliano"]
+print(nombre [1: ])
+print(nombre [1:5])
+print(nombre [-2])
