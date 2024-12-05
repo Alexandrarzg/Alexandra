eb97b142c0e28868b4ecad6f1efdebe04515637c
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:43:30 -0600

    Create programa12.py

---
+++ Codigo/bloque3/programa12.py
@@ -0,0 +1,7 @@
+#  P-12 metodo pop
+# 3/10/24
+# Alexandra Rodriguez Gomez
+frutas = ["manzana" , "uva", "cereza" , "sandia" , "naranja"]
+ultima_fruta = frutas.pop ()
+print(ultima_fruta)
+print(frutas)
