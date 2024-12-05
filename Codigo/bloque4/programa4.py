339633d54e7714f7fd3bfb829bf2f2e6511b3f58
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:46:46 -0600

    Create programa4.py

---
+++ Codigo/bloque4/programa4.py
@@ -0,0 +1,7 @@
+# P-4 programa que imprima el nombre de tus 10 personajes favoritos
+# 04/11/24
+# Alexandra Rodriguez Gomez
+personajes = ["Aladdin","Alicia","Aurora","Bella","Mérida","Mulán","Tiana","Wall-E","Cenicienta","Hércules"]
+
+for personaje in personajes:
+    print("El personaje de Disney es: ",personaje)
