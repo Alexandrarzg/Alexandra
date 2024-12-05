19597b24a40e9f415ee09317abee9a7921402aa5
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:54:54 -0600

    Create programa8.py

---
+++ Codigo/bloque5/programa8.py
@@ -0,0 +1,33 @@
+# P-8 agregar un ciclo while a p-7
+# 14/11/24
+# Alexandra Rodriguez Gomez
+
+def operaciones(num1, num2, digitos): 
+    suma = num1 + num2
+    resta = num1 - num2
+    multiplicacion = num1 * num2
+    division = float(num1 / num2)
+    modulo = num1 % num2
+    print(f'La suma de {num1} + {num2} es: {suma}')
+    print(f'La resta de {num1} - {num2} es: {resta}')
+    print(f'La multiplicacion de {num1} * {num2} es: {multiplicacion}')
+    print(f'La division de {num1} / {num2} es: {division:.{digitos}f}')
+    print(f'El modulo de {num1} % {num2} es: {modulo}\n\n')
+
+# Ciclo principal del programa
+while True:
+    # Solicitar datos al usuario
+    num1 = int(input('Ingrese el primer numero: '))
+    num2 = int(input('Ingrese el segundo numero: '))
+    digitos = int(input('Ingrese la cantidad de decimales a mostrar en la division: '))
+    cantidad = int(input("¿Cuántas veces deseas realizar las operaciones? "))
+
+    # Repetir las operaciones según lo indicado por el usuario
+    for _ in range(cantidad):
+        operaciones(num1, num2, digitos)
+
+    # Preguntar al usuario si desea continuar o salir
+    respuesta = input("¿Quieres realizar más operaciones? (escribe 'salir' para terminar o presiona Enter para continuar): ")
+    if respuesta.lower() == "salir":
+        print("¡Hasta luego!")
+        break
