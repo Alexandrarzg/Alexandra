3254bdfc3d5ce4e9f622c616069c548ec17d75d1
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:42:41 -0600

    Create programa11.py

---
+++ Codigo/bloque3/programa11.py
@@ -0,0 +1,8 @@
+# P-11 Agregando elementos en una lista
+# 31/10/24
+# Alexandra Rodriguez Gomez
+colores = ["rojo", "azul"]
+print(colores)
+colores.append("verde")
+print(colores)
+print(colores + "amarillo") # Marca error
