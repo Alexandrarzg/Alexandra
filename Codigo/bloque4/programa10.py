d2b437d1f1295449b0917a2d29d64cf670bc6c8e
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:48:58 -0600

    Create programa10.py

---
+++ Codigo/bloque4/programa10.py
@@ -0,0 +1,14 @@
+# P-10 Programa para demostrar el funcionamiento de los metodos upper() y lower()
+# 08/11/24
+# Alexandra Rodriguez Gomez
+
+# Definimos una cadena de texto
+cadena = "Python es un lenguaje de programaciòn"
+print("\n",cadena)
+
+cadena_mayusculas = cadena.upper() # Convertimos la cadena a mayusculas
+print("\n",cadena_mayusculas)
+
+cadena_minusculas = cadena.lower() # Convertimos la cadena a minusculas
+print("\n",cadena_minusculas)
+print("\nFin del programa")
