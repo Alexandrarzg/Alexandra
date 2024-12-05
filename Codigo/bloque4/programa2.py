0ecc384e6f9ca6e2293468516ab66ecca688f3e6
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:45:55 -0600

    Create programa2.py

---
+++ Codigo/bloque4/programa2.py
@@ -0,0 +1,12 @@
+# P-2 Introduccion al Ciclo FOR
+# 04/11/24
+# Alexandrqa Rodriguez Gomez
+nombres = ["Luis" , "Juan" , "Eduardo" , "Chuy" , "Marcos"]
+
+for nombre in nombres:
+    print("El nombre es " , nombre)
+print("\n")
+frutas = ["Manzana" , "Piña" , "Platano"]
+
+for fruta in frutas:
+    print("La fruta es: ",fruta)
