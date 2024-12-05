abc9eb04e05e38b534a958b992cc01021a7adb58
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:53:08 -0600

    Create programa4.py

---
+++ Codigo/bloque5/programa4.py
@@ -0,0 +1,14 @@
+# P-4 ejemplo de una funcion creada por el programador
+# 13/11/24
+# Alexandra Rodriguez Gomez
+
+# definicion de la funcion
+def saludar(nombre): # nombre es un parametro
+    print(f'\n¡Hola, {nombre}!')
+    print('¿Como estas?')
+    print('¡Espero que te encuentres bien!\n')
+    
+# Llamada a la funcion
+saludar('Edinguer') # Edinguer es un argumento
+saludar('Chuyito') # Chuyito es un argumento
+saludar('terricola') # terricola es un argumento
