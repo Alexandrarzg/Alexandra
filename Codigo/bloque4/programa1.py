ba65c0a7f72cc087323173827b06a1e0be8b7514
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:45:32 -0600

    Create programa1.py

---
+++ Codigo/bloque4/programa1.py
@@ -0,0 +1,13 @@
+# P-1 Ciclo For  
+# 04/11/24
+# Alexandrqa Rodriguez Gomez
+lista_num = [10 , 30 , 40 , 20 , 35 , 80]
+print(lista_num[0])
+print(lista_num[1])
+print(lista_num[2])
+print(lista_num[3])
+print(lista_num[4])
+print(lista_num[5])
+# Haciendo lo mismo pero con cilco for
+for i in lista_num:
+   print( i )
