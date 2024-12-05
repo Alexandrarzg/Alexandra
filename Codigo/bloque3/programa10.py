72c43f0ef02ac5112daba74acc35f2bc6aa783e5
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:41:57 -0600

    Create programa10.py

---
+++ Codigo/bloque3/programa10.py
@@ -0,0 +1,8 @@
+# P-10 lista Membership
+# 31/10/24
+# Alexandra Rodriguez Gomez
+nombres = ["Juan", "Pedro", "Emilio"]
+print("Juan" in nombres) #True
+print("Emi" in nombres) #False
+print("Javier" in nombres) #False
+print("Jose" not in nombres) #True
