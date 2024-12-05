5b3ee457b8f74e0290dffe90b8f1f54720830e02
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:46:23 -0600

    Create programa3.py

---
+++ Codigo/bloque4/programa3.py
@@ -0,0 +1,21 @@
+# P-3 Funcion rango
+# 04/11/24
+# Alexandrqa Rodriguez Gomez
+x = range(10)
+for num in x:
+    print(num)
+
+print("\n Imprime los valores del 5 al 15")    
+x1 = range(5, 16)
+for num in x1:
+    print(num)
+    
+print("\n Imprime los pares del del 10 al 20")
+x2 = range(10, 21, 2)
+for num in x2:
+    print(num)
+
+print("\n Imprime los impares del del 11 al 21")
+x3 = range(11, 22, 2)
+for num in x3:
+    print(num)
