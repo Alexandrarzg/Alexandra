d9c969e96409dfc024ac1b12969b5055803a8c78
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:31:11 -0600

    Create programa7.py

---
+++ Codigo/bloque3/programa7.py
@@ -0,0 +1,21 @@
+# P-7 Tipos de datos en las listas, crear un programa con 3 listas de elementos e imprima el tipo de dato
+# 30/10/2024
+# Alexandra Rodriguez Gomez
+
+cadenas = ["Juan", "Pedro", "Chuy"]
+print(cadenas) #muestra la lista completa
+print(cadenas[1]) #imprime "pedro"
+print(type(cadenas)) #imprime el tipo "list"
+print(type(cadenas[1])) #imprime el tipo "str"
+print(" \n ")
+enteros = [5, 4 , 8, 9]
+print(enteros) 
+print(enteros[3]) 
+print(type(enteros))
+print(type(enteros[3]))
+print(" \n ")
+booleanos = [True, False, True, False]
+print(booleanos) 
+print(booleanos[2]) 
+print(type(booleanos))
+print(type(booleanos[2]))
