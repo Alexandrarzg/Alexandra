2293b42fc10f7ca5147415efb7ff2e9cbef577a6
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:48:16 -0600

    Create programa8.py

---
+++ Codigo/bloque4/programa8.py
@@ -0,0 +1,20 @@
+# P-8 Estructuras while se ejecutan mientras la condicion que se encuentra sea verdadera
+# 07/11/24
+# Alexandra Rodriguez Gomez
+
+# Ejemplo 1
+# Imprimir los numeros del 1 al 10
+i = 1 # Inicializacion de la variable de control
+while i <= 10: # Condicion de parada
+    print(i)
+    i += 1 # Equivale a i = i + 1
+# Sintaxis:
+# while <condicion>:
+#    <Cuerpo del while>
+
+# Ejemplo 2
+# Imprimir los numeros del 10 al 1  
+i = 10
+while i >= 1:
+    print(i)
+    i-= 1
