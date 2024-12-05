a7dc0077d9cc2952ece4d09fc948590d5926f202
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:41:29 -0600

    Create programa9.py

---
+++ Codigo/bloque3/programa9.py
@@ -0,0 +1,15 @@
+# P-9 igualdad en las listas, compare puntos de todas las demas listas
+# 31/10/2024
+# Alexandra Rodriguez Gomez
+
+puntos =[10 , 50 , 20]
+puntos_2 =[10 , 50 , 20]
+ordenados =[10 , 20 , 50]
+puntos_texto =["10" , "50" , "20"]
+
+print = (puntos == puntos_2)
+print = (puntos == ordenados)
+print = (puntos_texto == puntos)
+print = (puntos != puntos_2)
+print = (puntos != ordenados)
+print = (puntos != puntos_texto)
