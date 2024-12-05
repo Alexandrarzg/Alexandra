9c92378e2685fd2aa72bc73bcf6ad3eeff41e065
Alexandrarzg <alexa123gomez@outlook.es>
2024-12-05 13:53:47 -0600

    Create programa5.py

---
+++ Codigo/bloque5/programa5.py
@@ -0,0 +1,14 @@
+# P-5 Argumento parametro
+# 13/11/24
+# Alexandra Rodriguez Gomez
+
+# definicion de la funcion
+def saludar(nombre, edad): # nombre es un parametro
+    print(f'\n¡Hola, {nombre}!')
+    print(f'Tienes {edad} años.')
+    print('¡Espero que te encuentres bien!\n')
+    
+# Llamada a la funcion
+saludar('Edinguer', 26) # Edinguer y 26 son  un argumento
+saludar('Chuyito', 18) # Chuyito y 18 son  un argumento
+saludar('terricola', 23) # terricola y 23 son  un argumento
